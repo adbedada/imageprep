2a9f88a4ae5dee1214c0587c5e2e22349c10ce1b
adbeda <adbedada@gmail.com>
2020-05-20 22:24:14 -0400

    create basic voc structure

--- imageprep/voc.py
+++ imageprep/voc.py
@@ -1,39 +1,70 @@
 import os
-from jinja2 import Environment, PackageLoader
+from imageprep import utils
+from xml.etree.ElementTree import Element, SubElement, tostring, XML
+from xml.etree import ElementTree
 
+from bs4 import BeautifulSoup
+#from ElementTree_pretty import prettify
 
-class Writer:
-    def __init__(self, path, width, height, depth=3, database='Unknown', segmented=0):
-        environment = Environment(loader=PackageLoader('imageprep', 'voc_template'), keep_trailing_newline=True)
-        self.annotation_template = environment.get_template('annotation.xml')
+# x = your xml
+#
+#
 
-        abspath = os.path.abspath(path)
+"""
+<annotation>
+	<folder>GeneratedData_Train</folder>
+	<filename>000001.png</filename>
+	<path>/my/path/GeneratedData_Train/000001.png</path>
+	<source>
+		<database>Unknown</database>
+	</source>
+	<size>
+		<width>224</width>
+		<height>224</height>
+		<depth>3</depth>
+	</size>
+	<segmented>0</segmented>
+	<object>
+		<name>21</name>
+		<pose>Frontal</pose>
+		<truncated>0</truncated>
+		<difficult>0</difficult>
+		<occluded>0</occluded>
+		<bndbox>
+			<xmin>82</xmin>
+			<xmax>172</xmax>
+			<ymin>88</ymin>
+			<ymax>146</ymax>
+		</bndbox>
+    </object>
+</annotation>
 
-        self.template_parameters = {
-            'path': abspath,
-            'filename': os.path.basename(abspath),
-            'folder': os.path.basename(os.path.dirname(abspath)),
-            'width': width,
-            'height': height,
-            'depth': depth,
-            'database': database,
-            'segmented': segmented,
-            'objects': []
-        }
+"""
 
-    def addObject(self, name, xmin, ymin, xmax, ymax, pose='Unspecified', truncated=0, difficult=0):
-        self.template_parameters['objects'].append({
-            'name': name,
-            'xmin': xmin,
-            'ymin': ymin,
-            'xmax': xmax,
-            'ymax': ymax,
-            'pose': pose,
-            'truncated': truncated,
-            'difficult': difficult,
-        })
+img_path = "../data/images/"
+label_path = "../data/labels/"
+out_path = "../data/voc/"
 
-    def save(self, annotation_path):
-        with open(annotation_path, 'w') as file:
-            content = self.annotation_template.render(**self.template_parameters)
-            file.write(content)
+img_arr = utils.images_as_array(img_path)
+
+files = os.listdir(img_path)
+for f in files:
+
+    im = utils.read_image(os.path.join(img_path,f))
+    width, height,depth = im.shape
+
+    root = Element("annotations")
+    folder = SubElement(root, "folder")
+    folder.text = img_path
+
+    fname = SubElement(root, "filename")
+    fname.text = str(f)
+
+    path = SubElement(root, "path")
+    path.text = str(os.path.join(img_path, f))
+
+    outfile = f[:-4] + '.xml'
+    tree = ElementTree.tostring(root)
+    output_file = open(out_path + outfile, "w")
+    output_file.write(str(tree))
+
