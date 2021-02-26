1cbbe7fc84caeb3de5a0c580c9325859720dfb93
adbeda <adbedada@gmail.com>
2021-02-26 14:02:45 -0500

    changed prep to class

--- imageprep/prep.py
+++ imageprep/prep.py
@@ -4,7 +4,6 @@ Raster data Preprocessing utility
 import numpy as np
 import glob
 import os.path
-import pandas as pd
 import geopandas as gpd
 import rasterio
 from rasterio import features
@@ -14,82 +13,86 @@ from pathlib import Path
 from imageprep import utils
 
 
-def list_raster_data(path):
-    """
-    Reads raster files from multiple folders and returns their names
-    :param path: directory path
-    :return: names of the raster files
-    """
+class ImageProcessor:
+    def __init__(self, path):
+        self.path = path
+        self.Image_list = []
 
-    images = glob.glob("{}/**/*.tif".format(path), recursive=True)
-    image_name = [os.path.basename(tif).split('.')[0]
-                  for tif in images]
+    def __len__(self):
+        return len(self.Image_list)
 
-    # single tiff files
-    if len(image_name) == 0:
-        image_name = [os.path.basename(path).split('.')[0]]
+    def __getitem__(self, item):
+        return self.Image_list[item]
 
-    return image_name
+    def list_raster_data(self):
+        """
+        Reads raster files from multiple folders and returns their names
+        :return: names of the raster files
+        """
 
+        images = glob.glob("{}/**/*.tif".format(self.path), recursive=True)
+        image_name = [os.path.basename(tif).split('.')[0]
+                      for tif in images]
 
-def read_raster_data(path):
-    """
-    Reads a set of associated raster bands from a file.
-    Can read one or multiple files stored in different folders.
-    :param path: file name or directory path
-    :return: raster files opened as GDALDataset
-    """
-    if os.path.isdir(path):
-        images = glob.glob("{}/**/*.tif".format(path), recursive=True)
-        raster_files = [gdal.Open(f, gdal.GA_ReadOnly) for f in images]
-    else:
-        raster_files = [gdal.Open(path, gdal.GA_ReadOnly)]
+        # single tiff files
+        if len(image_name) == 0:
+            image_name = [os.path.basename(self.path).split('.')[0]]
 
-    return raster_files
+        return image_name
 
+    def read_raster_data(self):
+        """
+        Reads a set of associated raster bands from a file.
+        Can read one or multiple files stored in different folders.
+        :return: raster files opened as GDALDataset
+        """
+        if os.path.isdir(self.path):
+            images = glob.glob("{}/**/*.tif".format(self.path), recursive=True)
+            raster_files = [gdal.Open(f, gdal.GA_ReadOnly) for f in images]
+        else:
+            raster_files = [gdal.Open(self.path, gdal.GA_ReadOnly)]
 
-def raster_to_array(path):
-    """
-    Converts images inside multiple folders to stacked array
-    :param path: directory path
-    :return: stacked numpy array
-    """
-    raster_array = np.stack([raster.ReadAsArray()
-                             for raster in read_raster_data(path)],
-                             axis=-1)
+        return raster_files
 
-    return raster_array
+    def raster_to_array(self):
+        """
+        Converts images inside multiple folders to stacked array
+        :return: stacked numpy array
+        """
+        raster_array = np.stack([raster.ReadAsArray()
+                                 for raster in self.read_raster_data()], axis=-1)
 
+        return raster_array
 
-def CreateTiff(Name, Array, driver, NDV, GeoT, Proj, DataType, path):
-    """
-    Converts array to a single or multi band raster file
-    :param Name: name of the output tiff file
-    :param Array: numpy array to be converted to
-    :param driver: output image (data) format
-    :param NDV: no Data Value (-9999)
-    :param GeoT: geographic transformation
-    :param Proj: projection
-    :param DataType: array data format
-    :return: GeoTiff
-    """
+    def create_tiff(Name, Array, driver, NDV, GeoT, Proj, DataType, path):
+        """
+        Converts array to a single or multi band raster file
+        :param Name: name of the output tiff file
+        :param Array: numpy array to be converted to
+        :param driver: output image (data) format
+        :param NDV: no Data Value (-9999)
+        :param GeoT: geographic transformation
+        :param Proj: projection
+        :param DataType: array data format
+        :return: GeoTiff
+        """
 
-    Array[np.isnan(Array)] = NDV
+        Array[np.isnan(Array)] = NDV
 
-    rows = Array.shape[1]
-    cols = Array.shape[0]
-    band = Array.shape[2]
-    noData = -9999
-    driver = gdal.GetDriverByName('GTiff')
-    Name_out = os.path.join(path,Name)
-    print('tif:'+ Name_out)
-    DataSet = driver.Create(Name_out, rows, cols, band, gdal.GDT_Float32)
-    DataSet.SetGeoTransform(GeoT)
-    DataSet.SetProjection(Proj)
+        rows = Array.shape[1]
+        cols = Array.shape[0]
+        band = Array.shape[2]
+        noData = -9999
+        driver = gdal.GetDriverByName('GTiff')
+        Name_out = os.path.join(path,Name)
+        print('tif:'+ Name_out)
+        DataSet = driver.Create(Name_out, rows, cols, band, gdal.GDT_Float32)
+        DataSet.SetGeoTransform(GeoT)
+        DataSet.SetProjection(Proj)
 
-    for i in range(band):
-        DataSet.GetRasterBand(i + 1).WriteArray(Array[:, :, i])
-        DataSet.GetRasterBand(i + 1).SetNoDataValue(noData)
+        for i in range(band):
+            DataSet.GetRasterBand(i + 1).WriteArray(Array[:, :, i])
+            DataSet.GetRasterBand(i + 1).SetNoDataValue(noData)
 
-    DataSet.FlushCache()
-    return Name
+        DataSet.FlushCache()
+        return Name
--- tests/test_prep.py
+++ tests/test_prep.py
@@ -1,24 +1,26 @@
 import os
-from imageprep import prep
-
+import numpy as np
+from imageprep.prep import ImageProcessor as IP
 
 cur_dir = os.path.dirname(__file__)
 # path to images and labels
 image_path = os.path.join(cur_dir, 'data', 'tiffs/')
 
+processor = IP(image_path)
 
 def test_image_names():
-    list_of_names = prep.list_raster_data(image_path)
+    list_of_names = processor.list_raster_data()
     assert len(list_of_names) == 4
     print(list_of_names)
 
 
 def test_read_raster_data():
-    gdal_dataset = prep.read_raster_data(image_path)
+    gdal_dataset = processor.read_raster_data()
     print(gdal_dataset)
 
 
 def test_raster_to_array():
-    raster_array = prep.raster_to_array(image_path)
-    print(raster_array)
+    raster_array = processor.raster_to_array()
+    assert isinstance(raster_array, np.ndarray)
+    print(np.shape(raster_array))
 
